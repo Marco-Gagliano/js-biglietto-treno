78ff5dd94c45b2d80d6adae1c8b9da4ef518f6a9
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-06 14:42:43 +0200

    js-scrittura-procedimento

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+/*
+BIGLIETTO DEL TRENO - PROCEDIMENTO:
+  1* L'utente dovrà inserire una cifra pari ai kilometri che dovrà percorrere e la sua età
+  2* il prezzo del biglietto sarà deciso da un calcolo ben definito (0,21€/km) 
+  3* verrà poi effettutato uno sconto in base all'età dell'utente se applicabile:20% se minorenne, 40% se over 65
+  4* l'importo finale verrà poi mostrato e nella sua completezza dei calcoli da far capire all'utente il perchè di quel prezzo 
+*/
+
