53ee9a6661c6149ef4a5d93bd798411df36bff4b
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-06 16:53:46 +0200

    calcolo-sconto-su-biglietto

--- JS/script.js
+++ JS/script.js
@@ -8,16 +8,54 @@ BIGLIETTO DEL TRENO - PROCEDIMENTO:
 
 let distanceKm = prompt ("Prego, inserisca il numero di km che deve percorrere")
 const priceForKm = 0.21;
-let totPriceKm = distanceKm * priceForKm 
+let totPriceKm = distanceKm * priceForKm
+let priceTicket = totPriceKm.toFixed(2)
 
-console.log(distanceKm, priceForKm, totPriceKm);
+console.log(distanceKm, priceForKm, totPriceKm, priceTicket);
 
-document.getElementById("biglietto").innerHTML =
+
+
+
+alert ("In base alla sua età, potrebbe essere un applicato uno sconto sul prezzo del biglieto: il 20% se ha meno di 18 anni, il 40% se ha più di 65 anni")
+
+let yourAge = prompt ("Prego, inserisca la età")
+
+if (yourAge < 18){
+  priceTicket = totPriceKm - (totPriceKm * 20 / 100);
+} else if (yourAge > 65){
+  priceTicket = totPriceKm - (totPriceKm * 40 / 100);
+} else{
+  priceTicket = totPriceKm;
+}
+
+const totalTicket = priceTicket.toFixed(2)
+
+console.log(totalTicket);
+
+
+document.getElementById("ticket-train").innerHTML =
+
 `
-<ul class="text-center">
+<div class="text-center">
+  <ul>
     <li><h3>Dati inseriti:</h3></li>
     <li>Numero Km: ${distanceKm} km</li>
     <li>Costo euro per Km: ${priceForKm} €/km</li>
-    <li>Prezzo totale del biglietto: ${totPriceKm} €</li>
+    <li>Prezzo del biglietto: ${priceTicket} €</li>
   </ul>
+
+
+  <ul>
+    <li><h3>Sconto Applicabile:<h3></li>
+    <li>Età inserita: ${yourAge} anni</li>
+    <li>Sconto del 20%: </li>
+    <li>Sconto del 40%: </li>
+  </ul>
+
+
+  <ul>
+    <li><h3>Prezzo Totale: ${totalTicket} €<h3></li>
+    <li><h5>Grazie per averci scelto. Buon Viaggio!<h5><li>
+  </ul>
+<div>
 `
